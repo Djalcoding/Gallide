b480fa4a2807cf6f07d52cb398cb6faad92e55fd
Djal Bertrand <100029664+Djalcoding@users.noreply.github.com>
2025-09-24 16:35:56 -0400

    Idk man

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
+pub mod read_ls;
+
 pub mod ui{
-    use tui::{backend::Backend, widgets::{List, ListItem}, Frame};
+    use tui::{backend::Backend, layout::{Constraint, Layout}, style::{Color, Style}, widgets::{Block, Borders, List, ListItem, ListState}, Frame};
 
-    pub fn build_directory_list() -> List<'static>{
-        let items = [ListItem::new("Ahke")];
+    
+    fn build_entries() -> Vec<ListItem<'static>>{
+        vec![ListItem::new("Ahke"), ListItem::new("Not Ahke")]
 
-        List::new(items)
     }
 
+    fn build_directory_list() -> List<'static>{
+        let items = build_entries();
 
-    pub fn build_ui<B: Backend>(f: &mut Frame<B>){
+        List::new(items)
+            .block(
+                Block::default()
+                .title("Directories")
+                .borders(Borders::ALL)
+            )
+            .style(Style::default().fg(Color::White))
+            .highlight_style(Style::default()
+                .fg(Color::Black)
+                .bg(Color::White)
+            )
+            .highlight_symbol("> ")
+    }
 
+    pub fn build_ui<B: Backend>(f: &mut Frame<B>, selected:usize){
+
+        let mut constraints = vec![Constraint::Percentage(50), Constraint::Percentage(50)];
+
+        let mut state:ListState = ListState::default();
+        let chunks = Layout::default()
+            .direction(tui::layout::Direction::Horizontal)
+            .margin(1)
+            .constraints(constraints)
+            .split(f.size());
+
+        state.select(Some(selected));
+
+        f.render_stateful_widget(build_directory_list(), chunks[0], &mut state);
+        f.render_widget(Block::default(), chunks[1]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
-use fzf_clone::ui;
-use termion::raw::IntoRawMode;
+use fzf_clone::{read_ls, ui};
+use termion::{clear, raw::IntoRawMode};
 use tui::{backend::TermionBackend, Terminal};
 
 fn main() -> Result<(), io::Error>{
@@ -9,12 +9,16 @@ fn main() -> Result<(), io::Error>{
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    let mut selected_id = 0;
+    println!("{}", clear::All);
+
+    read_ls::get_directories();
+    return Ok(());
     loop {
         let _ = terminal.draw(|f| {
-            ui::build_ui(f); // TODO: actually build ui
+            ui::build_ui(f,selected_id ); 
         });
 
-        if true{break;}
     }
     Ok(())
 }
